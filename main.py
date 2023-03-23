1168adc450b1ccac543271ab28e2ca970fc99a8c
Maksim <maxgubaref2017@yandex.ru>
2023-03-23 04:32:25 +0300

    already chatGPT bot/ v-0.1.1

--- main.py
+++ main.py
@@ -15,9 +15,7 @@ dp=Dispatcher(bot)
 
 @dp.message_handler()
 async def send(message: types.Message):
-    openai.api_key = os.getenv("OPENAI_API_KEY")
-
-    response = openai.Completion.create(
+        response = openai.Completion.create(
         model="text-davinci-003",
         prompt=message.text,
         temperature=0.5,
