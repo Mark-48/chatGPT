0be4c7967f890f7c795e51601f1038442f344bb3
Maksim <maxgubaref2017@yandex.ru>
2023-03-23 04:42:24 +0300

    already chatGPT bot/ v-0.2

--- main.py
+++ main.py
@@ -3,17 +3,16 @@ import telebot
 import aiogram
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
-import os
 from aiogram.utils import executor
-#from """control import printfunc
 
 token = '5586996454:AAEcrUpY12cWspCpjJ51E0kFShePIdgmB8A'
 openai.api_key = "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"
 
 bot = Bot(token)
+Bots = telebot.TeleBot('5586996454:AAEcrUpY12cWspCpjJ51E0kFShePIdgmB8A')
 dp=Dispatcher(bot)
 
-@dp.message_handler()
+@Bots.message_handler()
 async def send(message: types.Message):
         response = openai.Completion.create(
         model="text-davinci-003",
@@ -25,7 +24,7 @@ async def send(message: types.Message):
         presence_penalty=0.0,
         stop=["You:"]
     )
-        await message.answer(response['choices'][0]['text'])
+        await Bots.send_message(response['choices'][0]['text'])
 
         executor.start_polling(dp, skip_updates=True)
 
